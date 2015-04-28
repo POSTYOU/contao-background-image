1128f8101e422a0298e9a101e58a3ab5f20a2aed
Gerald Meier <garyee@gmx.de>
2015-04-28 12:58:43 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,16 +2,16 @@
 
 if (TL_MODE == 'FE') {
     $GLOBALS['TL_HOOKS']['parseFrontendTemplate'][] = array(
-        'postYou\\BackgroundImageModel',
+        'postyou\BackgroundImageModel',
         'parseFrontendTemplateHook'
     );
 
-    $GLOBALS['TL_HOOKS']['getContentElement'][] = array('postyou\\BackgroundImageModel', 'getContentElementHook');
+    $GLOBALS['TL_HOOKS']['getContentElement'][] = array('postyou\BackgroundImageModel', 'getContentElementHook');
 
 }
 
 $GLOBALS['TL_CONFIG']['bg-image']['fieldNr']="4";
 
-$GLOBALS['BE_FFL']['mobileImageWizard'] = 'MobileImageWizard';
+$GLOBALS['BE_FFL']['mobileImageWizard'] = 'postyou\MobileImageWizard';
 
 $GLOBALS['TL_HOOKS']['addCustomRegexp'][] = array('My_tl_content', 'myAddCustomRegexp');
