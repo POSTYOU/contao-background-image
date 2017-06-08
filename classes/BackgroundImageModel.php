5fb69000aedec5fd76df00428e897990dc9f26fa
theDyingMountain <mario_gates@ymail.com>
2017-06-08 14:42:16 +0200

    add backgroundimage css class

--- classes/BackgroundImageModel.php
+++ classes/BackgroundImageModel.php
@@ -13,20 +13,18 @@
 
 namespace postyou;
 
-
 class BackgroundImageModel extends \Frontend
 {
-    function parseFrontendTemplateHook($strBuffer, $tmpName)
+    public function parseFrontendTemplateHook($strBuffer, $tmpName)
     {
-
-         if (strpos($tmpName,"mod_article")!==false) {
+        if (strpos($tmpName, "mod_article")!==false) {
             if (preg_match_all("/id=\"(.*?)\"/", $strBuffer, $res)) {
                 if (!empty($res)) {
                     $alias = $res[1];
-                    if(preg_match("/\"/",$alias[0])){
-                        $alias_0 = str_replace('"','\"',$alias[0]); 
+                    if (preg_match("/\"/", $alias[0])) {
+                        $alias_0 = str_replace('"', '\"', $alias[0]);
                     } else {
-                        $alias_0 = $alias[0]; 
+                        $alias_0 = $alias[0];
                     }
                     $dbRes = $this->Database->prepare('SELECT id,addBackgroundImage,backgroundImageFilepath,backgroundImagePos FROM tl_article WHERE alias=? OR cssID REGEXP "(a:2:{i:0;s:)[0-9]+(:\")'.$alias[0].'(\")"')
                         ->execute($alias_0);
@@ -34,7 +32,7 @@ class BackgroundImageModel extends \Frontend
                     if ($dbRes["addBackgroundImage"] == 1 && isset($dbRes["backgroundImageFilepath"])) {
                         $objFile = \FilesModel::findByPk($dbRes["backgroundImageFilepath"]);
                         if ($objFile !== null && is_file(TL_ROOT . '/' . $objFile->path)) {
-                            $strBuffer=$this->insertSytleTag($objFile->path,deserialize($dbRes[backgroundImagePos],true),$strBuffer);
+                            $strBuffer=$this->insertSytleTag($objFile->path, deserialize($dbRes[backgroundImagePos], true), $strBuffer);
                         }
                     }
                 }
@@ -43,78 +41,76 @@ class BackgroundImageModel extends \Frontend
         return $strBuffer;
     }
 
-    function getContentElementHook($objElement, $strBuffer)
+    public function getContentElementHook($objElement, $strBuffer)
     {
-
         if ($objElement->row()["addBackgroundImage"] == '1' && isset($objElement->row()["backgroundImageFilepath"])) {
             $objFile = \FilesModel::findByPk($objElement->row()["backgroundImageFilepath"]);
-            $pos=deserialize($objElement->row()["backgroundImagePos"],true);
+            $pos=deserialize($objElement->row()["backgroundImagePos"], true);
             if ($objFile !== null && is_file(TL_ROOT . '/' . $objFile->path)) {
-                $strBuffer=$this->insertSytleTag($objFile->path,$pos,$strBuffer);
+                $strBuffer=$this->insertSytleTag($objFile->path, $pos, $strBuffer);
             }
-
         }
         return $strBuffer;
-
     }
 
-    function insertSytleTag($sourceFilePath,$pos,$strBuffer){
-        if($GLOBALS['TL_CONFIG']['mobileBackgroundImage']===true)
+    public function insertSytleTag($sourceFilePath, $pos, $strBuffer)
+    {
+        if ($GLOBALS['TL_CONFIG']['mobileBackgroundImage']===true) {
             $resolutions=(unserialize($GLOBALS['TL_CONFIG']['imageList']));
+        }
         $path_parts = pathinfo($sourceFilePath);
         $rID=uniqid("_".$path_parts['filename']."_");
 
-        $strBuffer = preg_replace("/class=\"/", "class=\"" .$rID." ", $strBuffer, 1);
+        $strBuffer = preg_replace("/class=\"/", "class=\"" .$rID." background-image ", $strBuffer, 1);
         $tmpBuffer= "<style scoped='scoped' type='text/css'>\n.".$rID." {background-image:url(".$sourceFilePath.");\n";
-        if(!empty($pos)){
-
-            foreach($pos as $key=>$attribute){
-                if(isset($attribute) && !empty($attribute)){
-                    if($key==0 && (!empty($attribute) || !empty($pos[1])) && empty($pos[2])) {
+        if (!empty($pos)) {
+            foreach ($pos as $key=>$attribute) {
+                if (isset($attribute) && !empty($attribute)) {
+                    if ($key==0 && (!empty($attribute) || !empty($pos[1])) && empty($pos[2])) {
                         $tmpBuffer .= "background-position:" . $attribute . " " . $pos[1] . ";\n";
                     }
-                    if($key==2 && !empty($attribute))
+                    if ($key==2 && !empty($attribute)) {
                         $tmpBuffer.="background-position:".$attribute.";\n";
-                    if($key==3)
+                    }
+                    if ($key==3) {
                         $tmpBuffer.="background-repeat:".$attribute.";\n";
-                    if($key==4)
+                    }
+                    if ($key==4) {
                         $tmpBuffer.="background-attachment:".$attribute.";\n";
+                    }
                 }
             }
         }
         $tmpBuffer.="}";
 
-        if(isset($resolutions) && is_array($resolutions) && !empty($resolutions)){ // mobile Versions are enabled and there is a list
-            foreach($resolutions as $value) {
-                if(!empty($value[1])){
-                $objFile = new \File($path_parts['dirname'] . "/" . $value[2] . $path_parts['filename'] . $value[3] . "." . $path_parts['extension'],true);
-                if ($objFile->exists()){ // file exists
+        if (isset($resolutions) && is_array($resolutions) && !empty($resolutions)) { // mobile Versions are enabled and there is a list
+            foreach ($resolutions as $value) {
+                if (!empty($value[1])) {
+                    $objFile = new \File($path_parts['dirname'] . "/" . $value[2] . $path_parts['filename'] . $value[3] . "." . $path_parts['extension'], true);
+                    if ($objFile->exists()) { // file exists
                     $tmpBuffer .= "\n@media screen and (";
-                    if ($value[0] == 1) {
-                        $tmpBuffer .= "min";
-                    } else {
-                        $tmpBuffer .= "max";
-                    }
-                    $tmpBuffer .= "-width: " . $value[1] . "px){
+                        if ($value[0] == 1) {
+                            $tmpBuffer .= "min";
+                        } else {
+                            $tmpBuffer .= "max";
+                        }
+                        $tmpBuffer .= "-width: " . $value[1] . "px){
                                     .".$rID." {background-image:url(" . $path_parts['dirname'] . "/" . $value[2] . $path_parts['filename'] . $value[3] . "." . $path_parts['extension'] . ")}
                                 }";
-                }
+                    }
                 }
             }
-
         }
         $tmpBuffer.='</style>';
         //return $tmpBuffer.$strBuffer;
-        $strBuffer =preg_replace("/>/",">\n".$tmpBuffer,$strBuffer,1);
+        $strBuffer =preg_replace("/>/", ">\n".$tmpBuffer, $strBuffer, 1);
         return $strBuffer;
     }
-    
+
     public function myAddCustomRegexp($strRegexp, $varValue, \Widget $objWidget)
     {
-        if ($strRegexp == 'px')
-        {
-            if (!preg_match('/^((\d{1,3}px) (\d{1,3}px)|(\d{1,3}%) (\d{1,3}%))$/', $varValue))
-            {
+        if ($strRegexp == 'px') {
+            if (!preg_match('/^((\d{1,3}px) (\d{1,3}px)|(\d{1,3}%) (\d{1,3}%))$/', $varValue)) {
                 $objWidget->addError('Field ' . $objWidget->label . ' muss zwei Pixel oder Prozent-Werte enthalten');
             }
 
@@ -123,5 +119,4 @@ class BackgroundImageModel extends \Frontend
 
         return false;
     }
-    
 }
